4562b6b1b827c692e0540304890e63620b848f6d
kida0 <kdwhy0425@gmail.com>
2023-06-20 10:48:29 +0900

    exclude TKAgg

--- train.py
+++ train.py
@@ -2,8 +2,8 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 import matplotlib.pyplot as plt
-import matplotlib
-matplotlib.use('TKAgg')
+# import matplotlib 
+# matplotlib.use('TKAgg') # vscode
 import seaborn as sns
 import numpy as np
 
